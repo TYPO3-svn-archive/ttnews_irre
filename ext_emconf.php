f733ab6f8a5a73eb1ca093362a684e62f02189b0
sorenmalling <sorenmalling@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-17 09:08:36 +0000

    [TASK] Initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ttnews_irre/trunk@46616 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ttnews_irre".
+#
+# Auto generated 17-04-2011 11:03
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'IRRE element for tt_news content',
+	'description' => 'Instead of one RTE field, you are able to add tt_content element as IRRE data for your news',
+	'category' => 'misc',
+	'author' => 'Soren Malling',
+	'author_email' => 'soren@sorenmalling.me',
+	'shy' => '',
+	'dependencies' => 'tt_news',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'tt_news' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"fcd3";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"1e76";s:20:"doc/wizard_form.html";s:4:"7354";}',
+);
+
+?>
